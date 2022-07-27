029c6f9790379ea89e6312af6dcf5f520de1a9ca
Jan Moeller <jan.moeller0@gmail.com>
2022-07-27 09:15:27 +0200

    feat(nix): configure systemd-resolved dns servers

--- nix/config/configuration.nix
+++ nix/config/configuration.nix
@@ -48,9 +48,28 @@
     shells = [pkgs.zsh];
   };
 
-  services.resolved.enable = true;
+  services.resolved = {
+    enable = true;
+    dnssec = "yes";
+    fallbackDns = [
+      "8.8.8.8"
+      "8.8.4.4"
+      "2001:4860:4860::8888"
+      "2001:4860:4860::8844"
+    ];
+
+    extraConfig = ''
+      DNSOverTLS=yes
+    '';
+  };
   networking = {
     useNetworkd = true;
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
     wireless.iwd = {
       enable = true;
       settings = {
