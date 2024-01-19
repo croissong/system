127de3ac214bd8213eaf566775301b4dd79fec45
Jan Moeller <jan.moeller0@gmail.com>
2024-01-19 23:38:09 +0100

    feat(nixos): configure default fonts

--- nix-config/nixos/fonts.nix
+++ nix-config/nixos/fonts.nix
@@ -1,6 +1,18 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    font-awesome
-    hack-font
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      font-awesome
+      hack-font
+      noto-fonts
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = ["Noto Serif"];
+        sansSerif = ["Noto San"];
+        monospace = ["Hack"];
+      };
+    };
+  };
 }
