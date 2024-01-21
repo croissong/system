87a459ed9cd6219fc133021eb90d8589fe6a9b2a
Jan Moeller <jan.moeller0@gmail.com>
2024-01-21 23:33:58 +0100

    feat: enable tlp svc

--- nix-config/nixos/services.nix
+++ nix-config/nixos/services.nix
@@ -8,6 +8,10 @@
 
     fwupd.enable = true;
 
+    # TODO: maybe https://github.com/AdnanHodzic/auto-cpufreq
+    # see https://nixos.wiki/wiki/Laptop
+    tlp.enable = true;
+
     # TODO: use nixos services + declarative config
     # https://nixos.wiki/wiki/Syncthing
     # syncthing = {
