7f1ff729dd2b0bf4a8499630f0630dc53ceed6b3
Jan Moeller <jan.moeller0@gmail.com>
2024-02-02 17:17:43 +0100

    fix(hm): fix env var config dir interpolations

--- nix-config/hm/env.nix
+++ nix-config/hm/env.nix
@@ -5,7 +5,7 @@
 }: {
   home.sessionVariables = rec {
     BROWSER = "firefox";
-    CARGO_HOME = "$XDG_DATA_HOME/cargo";
+    CARGO_HOME = "${config.xdg.dataHome}/cargo";
     DIFFPROG = "delta";
     DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
     DOT = "${config.home.homeDirectory}/dot";
@@ -17,25 +17,25 @@
     AGE_RECIPIENTS_FILE = "${config.xdg.configHome}/age/public-key.txt";
     SOPS_AGE_KEY_FILE = AGE_IDENTITY_FILE;
     SUMMON_PROVIDER = "${pkgs.gopass-summon-provider.outPath}/bin/gopass-summon-provider";
-    TF_CLI_CONFIG_FILE = "$XDG_CONFIG_HOME/terraform/terraformrc";
+    TF_CLI_CONFIG_FILE = "${config.xdg.configHome}/terraform/terraformrc";
     VISUAL = "emacsclient -c";
     WOBSOCK = "$XDG_RUNTIME_DIR/wob.sock";
 
     # bin overrides
     CARGO_BIN = "${CARGO_HOME}/bin";
-    KREW_ROOT = "$XDG_STATE_HOME/.krew";
+    KREW_ROOT = "${config.xdg.stateHome}/.krew";
     NIMBLE_BIN = "$HOME/.nimble/bin";
     PATH = "$PATH:$GOPATH/bin:$KREW_ROOT/bin:$NIMBLE_BIN:$CARGO_BIN";
 
     # dot overrides sponsored by xdg-ninja
-    AZURE_CONFIG_DIR = "$XDG_DATA_HOME/azure";
-    GRADLE_USER_HOME = "$XDG_DATA_HOME/gradle";
-    GRIPHOME = "$XDG_CONFIG_HOME/grip";
-    DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker";
-    ELECTRUMDIR = "$XDG_DATA_HOME/electrum";
-    RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
-    VAGRANT_HOME = "$XDG_DATA_HOME/vagrant";
-    PSQL_HISTORY = "$XDG_DATA_HOME/psql_history";
+    AZURE_CONFIG_DIR = "${config.xdg.dataHome}/azure";
+    GRADLE_USER_HOME = "${config.xdg.dataHome}/gradle";
+    GRIPHOME = "${config.xdg.configHome}/grip";
+    DOCKER_CONFIG = "${config.xdg.configHome}/docker";
+    ELECTRUMDIR = "${config.xdg.dataHome}/electrum";
+    RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
+    VAGRANT_HOME = "${config.xdg.dataHome}/vagrant";
+    PSQL_HISTORY = "${config.xdg.dataHome}/psql_history";
   };
 
   xdg = {
