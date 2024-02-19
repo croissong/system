afaaa38fb440f315c468b8f2a9f923a99d89b480
Jan Moeller <jan.moeller0@gmail.com>
2024-02-19 21:47:42 +0100

    refactor(hm): group program specific configs together

--- nix-config/hm/services.nix
+++ nix-config/hm/services.nix
@@ -9,6 +9,8 @@
     kdeconnect.enable = true;
 
     keybase.enable = true;
+    # don't auto start at boot
+    systemd.user.services.keybase.Install.WantedBy = lib.mkForce [];
 
     spotifyd = {
       enable = true;
@@ -31,44 +33,35 @@
     };
   };
 
-  systemd.user = {
-    services = {
-      # don't auto start at boot
-      keybase.Install.WantedBy = lib.mkForce [];
-
-      # https://github.com/francma/wob/tree/master/contrib/systemd
-      wob = {
-        Unit = {
-          Description = "A lightweight overlay volume/backlight/progress/anything bar for Wayland";
-          PartOf = "graphical-session.target";
-          After = "graphical-session.target";
-          ConditionEnvironment = "WAYLAND_DISPLAY";
-        };
-
-        Service = {
-          StandardInput = "socket";
-          ExecStart = "${pkgs.wob}/bin/wob";
-        };
-
-        Install = {
-          WantedBy = ["graphical-session.target"];
-        };
-      };
+  # https://github.com/francma/wob/tree/master/contrib/systemd
+  systemd.user.services.wob = {
+    Unit = {
+      Description = "A lightweight overlay volume/backlight/progress/anything bar for Wayland";
+      PartOf = "graphical-session.target";
+      After = "graphical-session.target";
+      ConditionEnvironment = "WAYLAND_DISPLAY";
     };
-    sockets = {
-      wob = {
-        Socket = {
-          ListenFIFO = "%t/wob.sock";
-          SocketMode = "0600";
-          RemoveOnStop = "on";
-          # If wob exits on invalid input, systemd should NOT shove following input right back into it after it restarts
-          FlushPending = "yes";
-        };
 
-        Install = {
-          WantedBy = ["sockets.target"];
-        };
-      };
+    Service = {
+      StandardInput = "socket";
+      ExecStart = "${pkgs.wob}/bin/wob";
+    };
+
+    Install = {
+      WantedBy = ["graphical-session.target"];
+    };
+  };
+  systemd.user.sockets.wob = {
+    Socket = {
+      ListenFIFO = "%t/wob.sock";
+      SocketMode = "0600";
+      RemoveOnStop = "on";
+      # If wob exits on invalid input, systemd should NOT shove following input right back into it after it restarts
+      FlushPending = "yes";
+    };
+
+    Install = {
+      WantedBy = ["sockets.target"];
     };
   };
 }
