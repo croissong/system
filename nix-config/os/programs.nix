8b6d805decb5f7693471295e4e5f05171b1d7121
Jan Moeller <jan.moeller0@gmail.com>
2024-02-25 00:34:06 +0100

    feat(os): add steam

--- nix-config/os/programs.nix
+++ nix-config/os/programs.nix
@@ -1,4 +1,9 @@
 {...}: {
   programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
   };
 }
