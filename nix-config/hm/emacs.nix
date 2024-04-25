d9c9aad19c6883f316207965ec1e8ca19ed84fc0
Jan Moeller <jan.moeller0@gmail.com>
2024-04-25 21:32:35 +0200

    chore(emacs): non-working DISPLAY workarounds

--- nix-config/hm/emacs.nix
+++ nix-config/hm/emacs.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   package = with pkgs;
     (emacsPackagesFor emacs-overlay.emacs-pgtk).emacsWithPackages (
       epkgs:
@@ -14,7 +18,14 @@ in {
     package = package;
     client.enable = true;
     defaultEditor = true;
+    # startWithUserSession = "graphical";
   };
+
+  # Note: not working
+  # required to make $DISPLAY env var available (w/o exec-path-from-shell)
+  # `startWithUserSession = "graphical"`, i.e. "graphical-session.target", does not work (too soon i guess)
+  systemd.user.services.emacs.Unit.After = lib.mkForce ["sway-session.target"];
+  systemd.user.services.emacs.Install.WantedBy = lib.mkForce ["sway-session.target"];
 
   home.packages = [package pkgs.emacs-lsp-booster];
 }
