1ab9f3fa92f92cbe46cb8dae0cc1edf3696e46c2
Jan Moeller <jan.moeller0@gmail.com>
2024-06-12 00:52:19 +0200

    chore(hm): adapt mpv wrapper function to recent nix changes

--- nix-config/hm/mpv.nix
+++ nix-config/hm/mpv.nix
@@ -2,7 +2,7 @@
   programs.mpv = {
     enable = true;
 
-    package = pkgs.wrapMpv pkgs.mpv-unwrapped {youtubeSupport = true;};
+    package = pkgs.mpv.override {youtubeSupport = true;};
     bindings = {
       WHEEL_UP = "ignore";
       WHEEL_DOWN = "ignore";
