d715e3c5fca68ec01a48cc487d1802bb68e5c955
Jan Moeller <jan.moeller0@gmail.com>
2024-07-05 20:29:32 +0200

    refactor(backup): group units

--- nix-config/hm/backup.nix
+++ nix-config/hm/backup.nix
@@ -3,81 +3,92 @@
   lib,
   pkgs,
   ...
-}: {
-  systemd.user = {
-    services = {
-      backup = {
-        Unit = {
-          Description = "resticprofile backup";
-          After = "network-online.target";
-          Wants = "network-online.target";
-        };
-
-        Service = {
-          Type = "oneshot";
-          ExecStart = "${lib.getExe pkgs.nixpkgs-pr-2.resticprofile} backup";
-          Environment = "PATH=$PATH:${with pkgs;
-            lib.makeBinPath [
-              bash
-              coreutils
-              fd
-              git
-              ripgrep
-            ]}";
-          ExecStopPost = "${pkgs.service-status.out}/bin/service-status backup";
-        };
+}: let
+  backup = {
+    service = {
+      Unit = {
+        Description = "resticprofile backup";
+        After = "network-online.target";
+        Wants = "network-online.target";
       };
 
-      "gitwatch-notes" = {
-        Unit = {
-          Description = "Gitwatch notes";
-        };
-        Service = {
-          ExecStart = ''
-            ${pkgs.gitwatch}/bin/gitwatch -s 600 -m "chore: update notes" ${config.home.homeDirectory}/dot/notes/
-          '';
-          ExecStop = "/bin/true";
-        };
-
-        Install = {
-          WantedBy = ["default.target"];
-        };
-      };
-
-      "gitwatch-docs" = {
-        Unit = {
-          Description = "Gitwatch docs";
-        };
-        Service = {
-          ExecStart = ''
-            ${pkgs.gitwatch}/bin/gitwatch -s 600 -m "chore: update docs" ${config.home.homeDirectory}/dot/docs/
-          '';
-          ExecStop = "/bin/true";
-        };
-
-        Install = {
-          WantedBy = ["default.target"];
-        };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${lib.getExe pkgs.nixpkgs-pr-2.resticprofile} backup";
+        Environment = "PATH=$PATH:${with pkgs;
+          lib.makeBinPath [
+            bash
+            coreutils
+            fd
+            git
+            ripgrep
+          ]}";
+        ExecStopPost = "${pkgs.service-status.out}/bin/service-status backup";
       };
     };
 
-    timers = {
-      backup = {
-        Unit = {
-          Description = "resticprofile backup";
-        };
-
-        Timer = {
-          Unit = "backup.service";
-          OnCalendar = "0/8:00:00";
-          AccuracySec = "10min";
-          Persistent = true;
-        };
-
-        Install = {
-          WantedBy = ["timers.target"];
-        };
+    timer = {
+      Unit = {
+        Description = "resticprofile backup";
       };
+
+      Timer = {
+        Unit = "backup.service";
+        OnCalendar = "0/8:00:00";
+        AccuracySec = "10min";
+        Persistent = true;
+      };
+
+      Install = {
+        WantedBy = ["timers.target"];
+      };
+    };
+  };
+
+  gitwatch = {
+    notes = {
+      Unit = {
+        Description = "Gitwatch notes";
+      };
+      Service = {
+        ExecStart = ''
+          ${pkgs.gitwatch}/bin/gitwatch -s 600 -m "chore: update notes" ${config.home.homeDirectory}/dot/notes/
+        '';
+        ExecStop = "/bin/true";
+      };
+
+      Install = {
+        WantedBy = ["default.target"];
+      };
+    };
+
+    docs = {
+      Unit = {
+        Description = "Gitwatch docs";
+      };
+      Service = {
+        ExecStart = ''
+          ${pkgs.gitwatch}/bin/gitwatch -s 600 -m "chore: update docs" ${config.home.homeDirectory}/dot/docs/
+        '';
+        ExecStop = "/bin/true";
+      };
+
+      Install = {
+        WantedBy = ["default.target"];
+      };
+    };
+  };
+in {
+  systemd.user = {
+    services = {
+      backup = backup.service;
+
+      gitwatch-notes = gitwatch.notes;
+      gitwatch-docs = gitwatch.docs;
+    };
+
+    timers = {
+      backup = backup.timer;
     };
   };
 }
