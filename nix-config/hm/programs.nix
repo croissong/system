848cc551e67216c8eafaa24667470401b52597e9
Jan Moeller <jan.moeller0@gmail.com>
2024-08-05 22:30:10 +0200

    feat(hm): add vscode

--- nix-config/hm/programs.nix
+++ nix-config/hm/programs.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   programs = {
     chromium = {
       enable = true;
@@ -10,5 +10,19 @@
     # disable command-not-found integration
     nix-index.enableFishIntegration = false;
     command-not-found.enable = false;
+
+    vscode = {
+      enable = true;
+      package = pkgs.vscode.fhs;
+      mutableExtensionsDir = false;
+      extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "slint";
+          publisher = "Slint";
+          version = "1.7.1";
+          hash = "sha256-5fxiN1WFdpFhiQxDgwuwdtNi3eCl/MjEEhN3xREqf70=";
+        }
+      ];
+    };
   };
 }
