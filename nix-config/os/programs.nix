39588e20562d81887db8ea65fb01ce16bcda9bc6
Jan Moeller <jan.moeller0@gmail.com>
2024-08-05 22:31:04 +0200

    feat(os): enable nix-ld

--- nix-config/os/programs.nix
+++ nix-config/os/programs.nix
@@ -5,5 +5,7 @@
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
+
+    nix-ld.enable = true;
   };
 }
