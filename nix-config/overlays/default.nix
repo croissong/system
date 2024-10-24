28facb9c6fd40b2e363b0545ebc7ad9362e110f6
Jan Moeller <jan.moeller0@gmail.com>
2024-10-24 10:50:27 +0200

    feat: mv rust overlays to separate file; rm gitwatch overlay

--- nix-config/overlays/default.nix
+++ nix-config/overlays/default.nix
@@ -24,6 +24,7 @@ let
 in
 generatedImports
 // {
+
   # This one brings our custom packages from the 'pkgs' directory
   additions =
     final: _prev:
@@ -33,58 +34,69 @@ generatedImports
       inherit inputs versions;
     };
 
-  modifications = final: prev: {
-    flashrom = import ./flashrom.nix { inherit final prev; };
+  modifications =
+    final: prev:
+    let
+      rustOverlay = import ./rust.nix { inherit final prev; };
+    in
+    rustOverlay
+    // {
 
-    kubeswitch = prev.kubeswitch.overrideAttrs (_: {
-      version = "0.9.1-next";
-      src = prev.fetchFromGitHub {
-        owner = "danielfoehrKn";
-        repo = "kubeswitch";
-        rev = "master";
-        hash = "sha256-kkrsbhR+OGB5VoaJxIIcN6Wr9c5nqub8uvcof+ALgwA=";
+      imports = [
+        ./rust.nix
+      ];
+
+      flashrom = import ./flashrom.nix { inherit final prev; };
+
+      kubeswitch = prev.kubeswitch.overrideAttrs (_: {
+        version = "0.9.1-next";
+        src = prev.fetchFromGitHub {
+          owner = "danielfoehrKn";
+          repo = "kubeswitch";
+          rev = "master";
+          hash = "sha256-/vN4oh5dyqHDiBAMV4Zt9PWoIXmlc5LWOMsW1npz9Fc=";
+        };
+
+        doCheck = false;
+      });
+
+      gitwatch = final.writeShellApplication {
+        name = "gitwatch";
+        runtimeInputs = with final.pkgs; [
+          git
+          inotify-tools
+          openssh
+          coreutils
+          gnugrep
+          gnused
+
+          # for custom pre-commit hook
+          bash
+          eza
+        ];
+
+        bashOptions = [
+          "errexit"
+          "pipefail"
+        ];
+        checkPhase = "";
+        text = builtins.readFile (
+          builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/croissong/gitwatch/master/gitwatch.sh";
+          }
+        );
       };
 
-      doCheck = false;
-    });
-
-    gitwatch = final.writeShellApplication {
-      name = "gitwatch";
-      runtimeInputs = with final.pkgs; [
-        git
-        inotify-tools
-        openssh
-        coreutils
-        gnugrep
-        gnused
-
-        # for custom pre-commit hook
-        bash
-        eza
-      ];
-
-      bashOptions = [
-        "errexit"
-        "pipefail"
-      ];
-      checkPhase = "";
-      text = builtins.readFile (
-        builtins.fetchurl {
-          url = "https://raw.githubusercontent.com/gitwatch/gitwatch/master/gitwatch.sh";
-        }
-      );
+      termdown = prev.termdown.overrideAttrs (_: rec {
+        version = "1.18.0";
+        src = final.fetchFromGitHub {
+          rev = version;
+          sha256 = "sha256-Hnk/MOYdbOl14fI0EFbIq7Hmc7TyhcZWGEg2/jmNJ5Y=";
+          repo = "termdown";
+          owner = "trehn";
+        };
+      });
     };
-
-    termdown = prev.termdown.overrideAttrs (_: rec {
-      version = "1.18.0";
-      src = final.fetchFromGitHub {
-        rev = version;
-        sha256 = "sha256-Hnk/MOYdbOl14fI0EFbIq7Hmc7TyhcZWGEg2/jmNJ5Y=";
-        repo = "termdown";
-        owner = "trehn";
-      };
-    });
-  };
 
   emacs-overlay = final: _prev: {
     emacs-overlay = import inputs.nixpkgs {
---
+++ nix-config/overlays/rust.nix
@@ -0,0 +1,33 @@
+{
+  final,
+  prev,
+}:
+let
+in
+{
+
+  desed = prev.desed.override (oldAttrs: {
+    rustPlatform.buildRustPackage =
+      args:
+      final.rustPlatform.buildRustPackage (
+        args
+        // rec {
+          cargoHash = "sha256-3bkNirrvBOqIOhxD5xf3XsdaUMWFVT/lnN3gmZQIB9I=";
+
+          version = "1.2.2";
+          src = final.fetchFromGitHub {
+            owner = "SoptikHa2";
+            repo = "desed";
+            rev = "refs/tags/v${version}";
+            hash = "sha256-aKkOs8IhnHjoJkXq9ryGn9fN0AmZyVTHbD/Vano+Erw=";
+          };
+
+          # src = final.fetchCrate {
+          #   pname = args.pname;
+          #   version = version;
+          #   hash = "sha256-kioQvtHpKg4/oY5IWQd29dGkRnXNjvE0wKea1s7i5MA=";
+          # };
+        }
+      );
+  });
+}
