c160e9d1966caec97fb24bd588f9e103a7c8efaf
Jan Moeller <jan.moeller0@gmail.com>
2024-10-24 10:46:04 +0200

    fix(emacs): use startWithUserSession = "graphical" (works now); formatting

--- nix-config/hm/emacs.nix
+++ nix-config/hm/emacs.nix
@@ -2,31 +2,30 @@
   pkgs,
   lib,
   ...
-}: let
-  package = with pkgs;
+}:
+let
+  package =
+    with pkgs;
     (emacsPackagesFor emacs-overlay.emacs-pgtk).emacsWithPackages (
-      epkgs:
-        with epkgs; [
-          jinx
-          auctex
-          pdf-tools
-          treesit-grammars.with-all-grammars
-        ]
+      epkgs: with epkgs; [
+        jinx
+        auctex
+        pdf-tools
+        treesit-grammars.with-all-grammars
+      ]
     );
-in {
+in
+{
   services.emacs = {
     enable = true;
     package = package;
     client.enable = true;
     defaultEditor = true;
-    # startWithUserSession = "graphical";
+    startWithUserSession = "graphical";
   };
 
-  # Note: not working
-  # required to make $DISPLAY env var available (w/o exec-path-from-shell)
-  # `startWithUserSession = "graphical"`, i.e. "graphical-session.target", does not work (too soon i guess)
-  systemd.user.services.emacs.Unit.After = lib.mkForce ["sway-session.target"];
-  systemd.user.services.emacs.Install.WantedBy = lib.mkForce ["sway-session.target"];
-
-  home.packages = [package pkgs.emacs-lsp-booster];
+  home.packages = [
+    package
+    pkgs.emacs-lsp-booster
+  ];
 }
