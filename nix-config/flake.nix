977adb58f12e907872369ff2f648b177a918d1ca
Jan Moeller <jan.moeller0@gmail.com>
2025-03-03 08:06:17 +0100

    chore: update flake & lock

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -61,7 +61,7 @@
     };
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
