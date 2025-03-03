252efc6a56d85558722e4b3e1fb36be2a586f957
Jan Moeller <jan.moeller0@gmail.com>
2025-03-03 08:09:09 +0100

    chore: update flashrom overlay url & sha

--- nix-config/overlays/flashrom.nix
+++ nix-config/overlays/flashrom.nix
@@ -3,38 +3,42 @@
   final,
   prev,
 }:
-prev.flashrom.overrideAttrs (finalAttrs: prevAttrs: {
-  src = final.fetchzip {
-    url = "https://download.flashrom.org/releases/flashrom-v${finalAttrs.version}.tar.bz2";
-    hash = "sha256-rXwD8kpIrmmGJQu0NHHjIPGTa4+xx+H0FdqwAwo6ePg=";
-  };
+prev.flashrom.overrideAttrs (
+  finalAttrs: prevAttrs: {
+    src = final.fetchzip {
+      url = "https://download.flashrom.org/releases/flashrom-v${finalAttrs.version}.tar.xz";
+      hash = "sha256-uUXWtopl3KCBUX15GhPUWO10ob7azocnjiXKgvIRdlI=";
+    };
 
-  nativeBuildInputs =
-    prevAttrs.nativeBuildInputs
-    ++ (with final; [
-      meson
-      ninja
-    ]);
+    nativeBuildInputs =
+      prevAttrs.nativeBuildInputs
+      ++ (with final; [
+        meson
+        ninja
+      ]);
 
-  buildInputs =
-    prevAttrs.buildInputs
-    ++ (with final; [
-      cmocka
-    ]);
+    buildInputs =
+      prevAttrs.buildInputs
+      ++ (with final; [
+        cmocka
+      ]);
 
-  mesonFlags = [
-    "-Dprogrammer=auto"
-  ];
+    mesonFlags = [
+      "-Dprogrammer=auto"
+    ];
 
-  postInstall = let
-    udevRulesPath = "lib/udev/rules.d/flashrom.rules";
-  in ''
-    # After the meson build, the udev rules file is no longer present
-    # in the build dir, so we need to get it from $src and patch it
-    # again.
-    # There might be a better way to do this...
-    install -Dm644 $src/util/flashrom_udev.rules $out/${udevRulesPath}
-    substituteInPlace $out/${udevRulesPath} \
-      --replace 'GROUP="plugdev"' 'TAG+="uaccess", TAG+="udev-acl"'
-  '';
-})
+    postInstall =
+      let
+        udevRulesPath = "lib/udev/rules.d/flashrom.rules";
+      in
+      ''
+        # After the meson build, the udev rules file is no longer present
+        # in the build dir, so we need to get it from $src and patch it
+        # again.
+        # There might be a better way to do this...
+        install -Dm644 $src/util/flashrom_udev.rules $out/${udevRulesPath}
+        substituteInPlace $out/${udevRulesPath} \
+          --replace 'GROUP="plugdev"' 'TAG+="uaccess", TAG+="udev-acl"'
+      '';
+  }
+)
