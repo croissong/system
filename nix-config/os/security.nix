d0712cbf23ae0d9285d88cd3bdc00d8cd4860f7c
Jan Moeller <jan.moeller0@gmail.com>
2025-03-03 08:09:43 +0100

    feat(os): disable fprint

--- nix-config/os/security.nix
+++ nix-config/os/security.nix
@@ -1,27 +1,23 @@
-{lib, ...}: {
+{ lib, ... }:
+{
   security = {
     polkit.enable = true;
 
     pam.services = {
       sudo = {
         nodelay = true;
-        fprintAuth = true;
       };
 
       su = {
         logFailures = lib.mkForce false;
         nodelay = true;
-        fprintAuth = true;
       };
 
       polkit-1.nodelay = true;
 
       # https://nixos.wiki/wiki/Sway#Swaylock_cannot_be_unlocked_with_the_correct_password
-      swaylock = {};
+      swaylock = { };
     };
   };
 
-  services.fprintd = {
-    enable = true;
-  };
 }
