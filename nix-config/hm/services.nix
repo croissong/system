06bfa08035a7f4c7004c35ad2e9e1a19acf23d29
Jan Moeller <jan.moeller0@gmail.com>
2025-06-10 17:03:06 +0200

    feat(hm): enable wob via home-manager

--- nix-config/hm/services.nix
+++ nix-config/hm/services.nix
@@ -33,37 +33,9 @@
         shared = { };
       };
     };
-  };
 
-  # https://github.com/francma/wob/tree/master/contrib/systemd
-  systemd.user.services.wob = {
-    Unit = {
-      Description = "A lightweight overlay volume/backlight/progress/anything bar for Wayland";
-      PartOf = "graphical-session.target";
-      After = "graphical-session.target";
-      ConditionEnvironment = "WAYLAND_DISPLAY";
-    };
-
-    Service = {
-      StandardInput = "socket";
-      ExecStart = "${pkgs.wob}/bin/wob";
-    };
-
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
-  };
-  systemd.user.sockets.wob = {
-    Socket = {
-      ListenFIFO = "%t/wob.sock";
-      SocketMode = "0600";
-      RemoveOnStop = "on";
-      # If wob exits on invalid input, systemd should NOT shove following input right back into it after it restarts
-      FlushPending = "yes";
-    };
-
-    Install = {
-      WantedBy = [ "sockets.target" ];
+    wob = {
+      enable = true;
     };
   };
 }
