39338be890958319f60ee2e7108c10f39fa553a3
Jan Moeller <jan.moeller0@gmail.com>
2025-07-02 16:00:11 +0200

    minor: switch service-status pkgs in backup service; add OnBootSec

--- nix-config/hm/backup.nix
+++ nix-config/hm/backup.nix
@@ -23,13 +23,9 @@ let
             fd
             git
             ripgrep
-            unixtools.ping
           ]
         }";
-
-        # https://stackoverflow.com/questions/35805354/systemd-start-service-at-boot-time-after-network-is-really-up-for-wol-purpose
-        ExecStartPre = "/bin/sh -c 'while ! ping -c1 1.1.1.1; do sleep 1; done'";
-        ExecStopPost = "${lib.getExe pkgs.service-status} backup";
+        ExecStopPost = "${lib.getExe pkgs.je} backup";
       };
     };
 
@@ -43,6 +39,7 @@ let
         OnCalendar = "0/4:00:00";
         AccuracySec = "10min";
         Persistent = true;
+        OnBootSec = "10min";
       };
 
       Install = {
