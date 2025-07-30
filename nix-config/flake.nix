8d5f2b0b727420eaa61cc61410918c11d309351e
Jan Moeller <jan.moeller0@gmail.com>
2025-07-30 11:52:45 +0200

    chore: update lix

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -64,7 +64,7 @@
     };
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
