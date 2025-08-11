7ed4bc73aed802734d22a611f4abc2c328c34b6e
Jan Moeller <jan.moeller0@gmail.com>
2025-08-11 22:02:52 +0200

    feat(docs): use gitwatch module services

--- nix-config/hm/docs.nix
+++ nix-config/hm/docs.nix
@@ -1,47 +1,27 @@
 {
   config,
-  lib,
   inputs,
   pkgs,
   ...
 }:
-let
-  gitwatch =
-    let
-      mkService = dir: {
-        Unit = {
-          Description = "Gitwatch ${dir}";
-        };
-        Service = {
-          ExecStart = ''
-            ${
-              inputs.gitwatch-rs.packages.${pkgs.system}.default
-            }/bin/gitwatch watch --log-level=debug ${config.home.homeDirectory}/dot/${dir}/
-          '';
-          Environment = "PATH=$PATH:${
-            lib.makeBinPath [
-              pkgs.bash
-              pkgs.coreutils
-              pkgs.git
-              pkgs.aichat
-            ]
-          }";
-        };
-        Install = {
-          WantedBy = [ "default.target" ];
-        };
-      };
-    in
-    {
-      notes = mkService "notes";
-      docs = mkService "docs";
-    };
-in
 {
-  systemd.user = {
-    services = {
-      gitwatch-notes = gitwatch.notes;
-      gitwatch-docs = gitwatch.docs;
+
+  imports = [ inputs.gitwatch-rs.modules.gitwatch ];
+
+  services.gitwatch = {
+    notes = {
+      repo_path = "${config.home.homeDirectory}/dot/notes/";
+      args = [ "--log-level=debug" ];
+      extraPackages = with pkgs; [
+        bash
+        coreutils
+        git
+        aichat
+      ];
+    };
+    docs = {
+      repo_path = "${config.home.homeDirectory}/dot/docs/";
+      args = [ "--log-level=debug" ];
     };
   };
 }
