0a8ff8dfbff1242aa33dcfe8eeafed8ac8f8cd25
Jan Moeller <jan.moeller0@gmail.com>
2025-08-23 14:25:51 +0200

    feat: switch to auto-cpufreq from tlp

--- nix-config/os/power.nix
+++ nix-config/os/power.nix
@@ -1,12 +1,7 @@
-{ outputs, pkgs, ... }:
+{ ... }:
 {
   services = {
-    # TODO: maybe https://github.com/AdnanHodzic/auto-cpufreq
-    # see https://nixos.wiki/wiki/Laptop
-    tlp.enable = true;
-
-    upower = {
-      enable = true;
-    };
+    auto-cpufreq.enable = true;
+    upower.enable = true;
   };
 }
