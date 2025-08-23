2b6241869cf1d66c3880edd5b207ffab1e0eeb67
Jan Moeller <jan.moeller0@gmail.com>
2025-08-23 14:26:41 +0200

    chore(hm): update nix.gc attribute

--- nix-config/hm/nix.nix
+++ nix-config/hm/nix.nix
@@ -1,9 +1,10 @@
-{...}: {
+{ ... }:
+{
   nix = {
     # needed in addition to the OS one, to clean up the user profile
     gc = {
       automatic = true;
-      frequency = "weekly";
+      dates = "weekly";
       options = "--delete-older-than 30d";
     };
   };
